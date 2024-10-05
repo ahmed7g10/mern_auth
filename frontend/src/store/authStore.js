c8d3781427e7295aa6a6a204aa191afbf08097d3
ahmed7g10 <ahmedabdo1084@gmail.com>
2024-10-05 14:36:59 +0300

    vite fixed 44 times

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,6 +1,8 @@
 import {create} from "zustand"
 import axios from "axios"
 const API_URL=import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+console.log(API_URL);
+
 // const API_URL= "https://mern-auth-gnxe.onrender.com";
 axios.defaults.withCredentials = true;
 export const useAuthStore=create((set)=>({
